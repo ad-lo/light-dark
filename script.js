c9323e09e8ad03863fe589c07b8b6d6e88f1eb52
Chris Sayer <crichardsayer@gmail.com>
2022-10-09 12:47:20 -0400

    final draft

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function darkMode() {
   nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
   textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
   toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classlist.remove('fa-sun');
-  toggleIcon.children[1].classlist.add('fa-moon'); 
+  toggleIcon.children[1].classList.remove('fa-sun');
+  toggleIcon.children[1].classList.add('fa-moon'); 
   image1.src = 'img/undraw_proud_coder_dark.svg';
   image2.src = 'img/undraw_conceptual_idea_dark.svg';
   image3.src = 'img/undraw_feeling_proud_dark.svg';
@@ -25,8 +25,8 @@ function lightMode() {
   nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
   textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
   toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classlist.add('fa-sun');
-  toggleIcon.children[1].classlist.remove('fa-moon'); 
+  toggleIcon.children[1].classList.add('fa-sun');
+  toggleIcon.children[1].classList.remove('fa-moon'); 
   image1.src = 'img/undraw_proud_coder_light.svg';
   image2.src = 'img/undraw_conceptual_idea_light.svg';
   image3.src = 'img/undraw_feeling_proud_light.svg';
@@ -38,12 +38,25 @@ function lightMode() {
 function switchTheme(event) {
  if (event.target.checked) {
   document.documentElement.setAttribute('data-theme', 'dark');
+  localStorage.setItem('theme', 'dark');
   darkMode();
  } else {
   document.documentElement.setAttribute('data-theme', 'light');
+  localStorage.setItem('theme', 'light');
   lightMode();
  }
 }
 
 //  Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// chech local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
